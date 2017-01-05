6a32313d00005039ee0d49d80c89d8fc4abbe7be
Igor Konovalov <konovalov.avp@gmail.com>
2017-01-05 17:14:11 +0300

    fixed bug in clear button, step count (game of life)

--- assets/JS/Game_of_Life/index.js
+++ assets/JS/Game_of_Life/index.js
@@ -7,6 +7,7 @@ const rowCount = Math.floor(cx.canvas.height / scale);
 const columnCount = Math.floor(cx.canvas.width / scale);
 const color = "#0095DD";
 
+
 console.log("columns: " + columnCount + " rows: " + rowCount);
 
 
@@ -95,12 +96,21 @@ function drawGrid() { // рисуем решетку
   cx.stroke();
 }
 
+let step = 0;
+
+function drawStep() {
+  cx.font = "16px Arial";
+  cx.fillStyle = "#000";
+  cx.fillText("Step: " + step, 3, 15);
+}
+
 
 function updateField() {
   cx.clearRect(0, 0, cx.canvas.width, cx.canvas.height);
   drawBoxes(boxes);
   drawGrid();
-  console.log("обновляю");
+  drawStep();
+  // console.log("обновляю");
 }
 
 // логика
@@ -270,12 +280,16 @@ function isAlive(boxes, column, row) {
 
 }
 
+// рисуем количество шагов
+
+
 // кнопки
 
 const bNext = document.getElementById("step");
 bNext.addEventListener("click", function () {
   getNextStep(boxes)
   boxCopy(boxes, boxesNextStep);
+  step++;
   updateField();
   cleanNextStep(boxesNextStep);
 });
@@ -288,19 +302,23 @@ bRandom.addEventListener("click", function () {
 
 const bStart = document.getElementById("start");
 let isTimerOn = false;
+
 bStart.addEventListener("click", function () {
   if (!isTimerOn) {
     let speedValue = document.getElementById("speed").value;
     timer = setInterval(function() {
       getNextStep(boxes)
       boxCopy(boxes, boxesNextStep);
+      step++;
       updateField();
       cleanNextStep(boxesNextStep);
     }, speedValue * 100);
     isTimerOn = true;
+    return;
   } else {
     clearInterval(timer);
     isTimerOn = false;
+    return;
   }
 })
 
@@ -310,13 +328,14 @@ bPause.addEventListener("click", function () {
   console.log(timer);
   clearInterval(timer);
   isTimerOn = false;
-  }
+  } else return;
 })
 
 const bClear = document.getElementById("clear");
 bClear.addEventListener("click", function () {
+  step = 0;
   cleanNextStep(boxesNextStep);
-  boxCopy(boxes, boxesNextStep);
+  cleanNextStep(boxes);
   updateField();
   clearInterval(timer);
   isTimerOn = false;
